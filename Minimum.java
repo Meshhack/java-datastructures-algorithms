0117169a3babde67c54f0f3550114778a7948f20
Meshhack <meshnzai1@gmail.com>
2026-07-06 15:00:59 +0300

    finding minimum without Math.min

---
+++ Minimum.java
@@ -0,0 +1,18 @@
+public class Minimum {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int minValue = min(a, b);
+        System.out.println("The minimum value is: " + minValue);
+    }
+    public static int min(int a, int b) {
+         a = 4;
+         b = 5;
+        if (a < b) {
+            return a; 
+        }       else {
+            return b;
+    }
+
+
+}}
