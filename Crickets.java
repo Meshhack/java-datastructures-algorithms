b5c069e0d4753ed60fa9aafa326af18e8588559a
Meshhack <meshnzai1@gmail.com>
2026-07-06 14:32:05 +0300

    exams for the last chapter changes

--- Crickets.java
+++ Crickets.java
@@ -6,7 +6,7 @@ public class Crickets {
         double protein = 0.6; // grams of protein per gram of Crickets
         double proteinPerCricket = cricket * protein; // grams of protein per Crickets
         double proteinNeeded = (double) 200 / proteinPerCricket;                                              //
-        System.out.println("he needs " + proteinNeeded + " Crickets.");
+        System.out.printf("he needs %.2f Crickets.", proteinNeeded);
 
     }
 }
